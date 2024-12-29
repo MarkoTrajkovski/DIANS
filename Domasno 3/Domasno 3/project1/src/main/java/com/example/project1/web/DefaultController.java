7181e238c40cf561e717f8a9ca8c7aea76207c4e
nenad stefanovski <161628244+xx1078@users.noreply.github.com>
2024-12-29 18:09:57 +0100

    Add files via upload

---
+++ Domasno 3/Domasno 3/project1/src/main/java/com/example/project1/web/DefaultController.java
@@ -0,0 +1,20 @@
+package com.example.project1.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+}
